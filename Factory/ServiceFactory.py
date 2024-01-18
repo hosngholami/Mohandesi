b3ede1ce72ea083a9edb91f630771adab2047753
hosngholami <hosn.gholami76@gmail.com>
2024-01-18 14:46:25 +0330

    پیاده سازی الگوری فکتوری

--- Factory/ServiceFactory.py
+++ Factory/ServiceFactory.py
@@ -1,31 +1,51 @@
-from accounting.externalsystem.adapter import *
-import yaml
-import sys
-import types
 import importlib
+import yaml
+class Factory:
+    
+    instance = None
+    accounting_adapter = ""
+    inverntory_adapter = ""
+
+    
+    def __init__(self):
+        pass
 
 
-class AccouningtFactory():
+
+    @staticmethod
+    def getInstance(factory):
+        global instance
+        if factory.instance is None:
+            factory.instance = Factory()
+        return factory.instance
+
+
+
+
     def getInventoryAdapter(self):
         with open('shop/config/factory.yml', 'r') as file:
             data = yaml.safe_load(file)
             adpater_name =  data['inventory_adapter'] 
             module_name = data['inventory_module']
 
-        instance = getattr(importlib.import_module(module_name), adpater_name)
-        inventory_factory = instance()
-        return inventory_factory
+        inventory_factory = getattr(importlib.import_module(module_name), adpater_name)
+        return inventory_factory()
 
 
 
-    def getAccountingFactory(self):
+    def getAccountingAdapter(self):
 
         with open('shop/config/factory.yml', 'r') as file:
             data = yaml.safe_load(file)
             adpater_name =  data['accounting_adapter'] 
             module_name = data['accounting_module']
-            print(adpater_name)
+            
 
         instance = getattr(importlib.import_module(module_name), adpater_name)
         accounting_factory = instance()
         return accounting_factory
+    
+
+
+
+
--- emarket.py
+++ emarket.py
@@ -1,7 +1,73 @@
+import psycopg2
+from Factory.ServiceFactory import Factory
 
-from sales.servicefactory import AccouningtFactory
+
+class Shop:
+    def __init__(self, id, name):
+        self.id = id,
+        self.name = name
+
+
+
+def adapter():
+    factory = Factory.getInstance(Factory)
+    accounting =  factory.getAccountingAdapter()
+    print(accounting.sale(10, 20))
+
+state = {
+    'exit': 0,
+    'adapter': 1,
+}
+
 
 if __name__ == '__main__':
-    factory = AccouningtFactory()
-    accounting = factory.getAccountingFactory()
-    inventory = factory.getInventoryAdapter()
+    while True:
+        print('enter 0 for exit')
+        key = input('plese select model: ')
+        
+        match state[key]:
+            case 1:
+                adapter()
+            case 0:
+                exit(1)
+
+
+
+    # connection = psycopg2.connect(host="localhost", dbname="emarket", user="postgres", password="123", port=5432)
+    
+
+    # cursor = connection.cursor()
+
+
+    # cursor.execute("""CREATE TABLE IF NOT EXISTS shop (
+    #                     id INT PRIMARY KEY,
+    #                     name VARCHAR(50)
+    #                 )
+
+    #                """)
+
+
+    # connection.commit()
+    # # cursor.close()
+    # # connection.close()
+
+
+    # cursor = connection.cursor()
+
+    # cursor.execute("""
+    #             select *  from shop
+    #         """)
+    
+    # list_shop = []
+    # for item in cursor.fetchall():
+    #     list_shop.append(Shop(item[0], item[1]))
+
+
+    # for item in list(list_shop):
+    #     print(item.id)
+
+
+
+    # factory = AccouningtFactory()
+    # accounting = factory.getAccountingFactory()
+    # inventory = factory.getInventoryAdapter()
