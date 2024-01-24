62c4df7dcef8589e3d1d4eaefaa1ac1a4e5b8b28
hosngholami <hosn.gholami76@gmail.com>
2024-01-24 19:59:21 +0330

    تکمیل الگوری پراکسی

--- sales/proxy/adapter.py
+++ sales/proxy/adapter.py
@@ -1,17 +1,46 @@
 from sales.proxy.interface import *
-
+import time
+import random
 
 class TaxAdapter(IProductAdapter):
     def get_total(self, factors):
-        print('tax')
-        return 'tax'
+        count = 0
+        rand = random.randrange(1,3)
+        while True:
+            
+            try:
+                count += 1
+                if rand == 2:
+                    print("you connected to Tax adapter") 
+                if count == 5:
+                    raise Exception("TimeOut")
+
+            except:
+                print('tax adapter is faild')
+                return None
+        return None
+    
     
 
 
 class GoldTaxProAdapter(IProductAdapter):
     def get_total(self, factors):
-        print('tax pro')
-        return 'tax pro'
+        count = 0
+        rand = random.randrange(1,3)
+        while True:
+            
+            try:
+                count += 1
+                if rand == 2:
+                    print("you connected to Tax pro adapter ")
+                    break
+                if count == 5:
+                    raise Exception("TimeOut")
+
+            except:
+                print('tax pro adapter is faild')
+                return None
+        return None
 
 
 
--- sales/proxy/sales_proxy.py
+++ sales/proxy/sales_proxy.py
@@ -1,6 +1,7 @@
 from sales.proxy.interface import IProductAdapter
 from Factory.ServiceFactory import Factory
 from Factory import ServiceFactory
+import random
 class Proxy(IProductAdapter):
 
     iProxySale = IProductAdapter
@@ -11,8 +12,11 @@ class Proxy(IProductAdapter):
         for i in range(len(adapter)):
             instance = adapter[i]
             instance.get_total(10)
-            # iProxySale.get_total(10)
-        # adapter.get_total(20)
+            
+
+
+
+
 
     def get_total(self, factors):
         pass
